aa44ba98414e1c59d0f67ea0245e307d53c28532
RaulCiucalau <ciucalauraul@gmail.com>
2025-10-19 14:45:19 +0200

    feat: integrate PlayerComponent and enhance game logic for card handling

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,24 +1,49 @@
 import { Component } from '@angular/core';
 import { NgStyle, NgForOf, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [NgStyle, NgForOf, NgIf],
+  imports: [
+    NgStyle, 
+    NgForOf, 
+    NgIf, 
+    MatButtonModule, 
+    MatIconModule, 
+    PlayerComponent
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-  game: Game = new Game();
   pickCardAnimation = false;
+  currentCard = '';
+  game: Game = new Game();
+
+  constructor() { }
 
   ngOnInit(): void {
     this.newGame();
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      const card = this.game.stack.pop();
+      if (card) {
+        this.currentCard = card;
+        this.pickCardAnimation = true;
+
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+
   }
 
   newGame() {
