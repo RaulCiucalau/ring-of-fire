945fe97239f6fd2c5f341d36e861af0e6c6ee840
Raul Ciucalau <ciucalauraul@gmail.com>
2025-11-10 22:22:27 +0100

    fix: update current player index calculation after card draw

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -39,6 +39,8 @@ export class GameComponent {
         this.currentCard = card;
         this.pickCardAnimation = true;
 
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
         setTimeout(() => {
           this.game.playedCards.push(this.currentCard);
           this.pickCardAnimation = false;
