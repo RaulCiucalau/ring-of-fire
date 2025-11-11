a7178badc9ffc43af10932f4cb88ad7fab252a82
Raul Ciucalau <ciucalauraul@gmail.com>
2025-11-11 21:56:00 +0100

    fix: add MatCardModule and MatIconModule imports for enhanced component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatInputModule, MatButtonModule],
+  imports: [CommonModule, RouterOutlet, MatInputModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
